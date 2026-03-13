7be2a0e6063b651a9b68642ee055aa090e1f0227
Shriyabhavani <shriyabhavaninaram@gmail.com>
2026-03-13 18:36:10 +0530

    Update views.py

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -127,12 +127,12 @@ def _build_msal_app(cache=None, authority=None):
         client_credential=Config.CLIENT_SECRET,
         token_cache=cache,
     )
-    
+
 def _build_auth_url(authority=None, scopes=None, state=None):
     return _build_msal_app(authority=authority).get_authorization_request_url(
         scopes or [],
         state=state,
-        redirect_uri=url_for("authorized", _external=True)
+        redirect_uri=url_for("authorized", _external=True, _scheme="https")
     )
 
 
